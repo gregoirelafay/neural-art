7119788adacb5898e05d2fbb729b42e6c30bfd94
gregoire lafay <lafaygregoire@gmail.com>
2021-08-23 23:30:27 +0200

    update save_directory function in data.py, move directory_name creation out of the loop

--- neuralart/data.py
+++ neuralart/data.py
@@ -8,9 +8,9 @@ def save_csv(data, csv_path, filename):
 
 
 def save_directory(data, target, input_path, output_path, n=None):
+    directory_name = f"{os.path.basename(output_path)}-{target}-class_{data[target].nunique()}"
+    if n: directory_name = f"{directory_name}-n_{n}"
     for i, j in data.iterrows():
-        directory_name = f"{os.path.basename(output_path)}-{target}-class_{data[target].nunique()}"
-        if n: directory_name = f"{directory_name}-n_{n}"
         old_path = os.path.join(input_path, j.path)
         new_path = os.path.join(output_path, directory_name,
                                 eval(f"j.{target}"), j.title)
