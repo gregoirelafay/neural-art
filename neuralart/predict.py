f0d386e93ed1d7bbaf3e017f32dfe2d35ce9182d
Gregoire Lafay <lafaygregoire@gmail.com>
2021-08-30 17:57:09 +0200

    added functions to predict.py

--- neuralart/predict.py
+++ neuralart/predict.py
@@ -1,21 +1,28 @@
 import tensorflow as tf
 from tensorflow.keras import models
+import numpy as np
+
 class Predict():
     def __init__(self, image):
         self.image = image
-        self.model_path
+        self.model = None
+        self.model_path = None
+        self.decoded_image = None
+        self.img_height = None
+        self.img_width = None
 
     def decode_image(self, img_height, img_width):
-        img = tf.io.decode_image(self.image, channels=3)
-        self.decode_image = tf.image.resize(self.image, [img_height, img_width])
+        self.img_height = img_height
+        self.img_width = img_width
 
-    def preprocess_image(self, model_name):
-        models.load_model(filepath)
+        img = tf.io.decode_png(tf.constant(self.image), channels=3)
+        self.decoded_image = tf.image.resize(img,
+                                             [self.img_height, self.img_width])
 
+    def load_model(self,model_path):
+        self.model_path = model_path
+        self.model = models.load_model(model_path)
 
-    def load_model(model_path):
-        pass
-
-    def get_prediction(img):
-        model = load_model(model_path)
-        model.predict(tf.expand_dims(img, axis=0))
+    def get_prediction(self):
+        assert self.model, "Please load a model"
+        return self.model.predict(tf.expand_dims(self.decoded_image, axis=0))
