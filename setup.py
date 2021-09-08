d63f595b3b72257962d5bb286290e070bf0c8c06
gregoire lafay <lafaygregoire@gmail.com>
2021-09-09 00:05:50 +0200

    remove scripts=["scripts/neural-art-run"] from setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,4 @@ setup(name='neural-art',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/neural-art-run'],
       zip_safe=False)
